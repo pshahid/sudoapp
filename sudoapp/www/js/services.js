d3efa451d4cb43a0aee0547fbaec11b4ca7c2d6c
Paul Shahid <pshahid90@gmail.com>
2014-09-13 23:24:11 -0400

    Completed a bit of a refactor

--- sudoapp/www/js/services.js
+++ sudoapp/www/js/services.js
@@ -25,111 +25,94 @@ angular.module('starter.services', [])
   }
 })
 
-.factory('Events', function() {
-  // event testing data
-  // var events = [
-  //   {category: 'hosted',
-  //    events: [
-  //           { id: 0, 
-  //           name: 'Happy Hour', 
-  //           host: 'Paul Shahid', 
-  //           time: '5:00pm', 
-  //           date: 'September 14th', 
-  //           place: 'Big Gun', 
-  //           guests: '4',
-  //           description: 'Guys the burgers are half off until 6. They are delicious and we should go eat them.',
-  //           avatar: 'img/avatars/pjs.jpg'
-  //           }
-  //       ]},
+.factory('Events', function() {  // event testing data
+    var events = [
+        {
+            category: 'Your events',
+            events: [
+                { 
+                    id: 0, 
+                    name: 'Happy Hour', 
+                    host: 'Paul Shahid', 
+                    time: '5:00pm', 
+                    date: 'Sep 14', 
+                    place: 'Big Gun', 
+                    guests: '4',
+                    description: 'Guys the burgers are half off until 6. They are delicious and we should go eat them.',
+                    avatar: 'img/avatars/pjs.jpg'
+                }
+            ]
+        },
 
-  //       {category: 'invited',
-  //           events: [
-  //               { id: 1, 
-  //               name: 'Hang Out', 
-  //               host: 'Ron Ratatata', 
-  //               time: '9:00pm', 
-  //               date: 'September 14th', 
-  //               place: 'Smoking Lamp', 
-  //               guests: '2',
-  //               description: 'none',
-  //               avatar: 'img/avatars/ronak.jpg' 
-  //             },
-  //               { id: 2, 
-  //               name: 'Get Drunk', 
-  //               host: 'Luke Burrows', 
-  //               time: '7:00pm', 
-  //               date: 'September 14th', 
-  //               place: 'Bier Garten', 
-  //               guests: '19',
-  //               description: 'none',
-  //               avatar: 'img/avatars/luke.jpg' 
-  //               }
-  //           ]},
-  //       {category: 'public',
-  //           events: [
-  //               { id: 3, 
-  //               name: 'Barcamp', 
-  //               host: 'Ryan Scrolls', 
-  //               time: '8:00am', 
-  //               date: 'November 14th', 
-  //               place: 'Harbor Walk', 
-  //               guests: '8',
-  //               description: 'none',
-  //               avatar: 'img/avatars/ryry.jpg'
-  //               }
-  //           ]}
-  //       ];
-  
-  var events = [
-    { id: 0, 
-      name: 'Happy Hour', 
-      host: 'Paul Shahid', 
-      time: '5:00pm', 
-      date: 'September 14th', 
-      place: 'Big Gun', 
-      guests: '4',
-      description: 'Guys the burgers are half off until 6. They are delicious and we should go eat them.',
-      avatar: 'img/avatars/pjs.jpg'
-  },
-    { id: 1, 
-      name: 'Hang Out', 
-      host: 'Ron Ratatata', 
-      time: '9:00pm', 
-      date: 'September 14th', 
-      place: 'Smoking Lamp', 
-      guests: '2',
-      description: 'none',
-      avatar: 'img/avatars/ronak.jpg' 
-    },
-    { id: 2, 
-      name: 'Get Drunk', 
-      host: 'Luke Burrows', 
-      time: '7:00pm', 
-      date: 'September 14th', 
-      place: 'Bier Garten', 
-      guests: '19',
-      description: 'none',
-      avatar: 'img/avatars/luke.jpg' 
-    },
-    { id: 3, 
-      name: 'Barcamp', 
-      host: 'Ryan Scrolls', 
-      time: '8:00am', 
-      date: 'November 14th', 
-      place: 'Harbor Walk', 
-      guests: '8',
-      description: 'none',
-      avatar: 'img/avatars/ryry.jpg'
-    }
-  ];
+        {
+            category: 'Invited',
+            events: [
+                { 
+                    id: 1, 
+                    name: 'Hang Out', 
+                    host: 'Ron Ratatata', 
+                    time: '9:00pm', 
+                    date: 'Sep 14', 
+                    place: 'Smoking Lamp', 
+                    guests: '2',
+                    description: 'none',
+                    avatar: 'img/avatars/ronak.jpg' 
+                },
+                { 
+                    id: 2, 
+                    name: 'Get Drunk', 
+                    host: 'Luke Burrows', 
+                    time: '7:00pm', 
+                    date: 'Sep 14', 
+                    place: 'Bier Garten', 
+                    guests: '19',
+                    description: 'none',
+                    avatar: 'img/avatars/luke.jpg' 
+                }
+            ]
+        },
+        {
+            category: 'Public',
+            events: [
+                { 
+                    id: 3, 
+                    name: 'Barcamp', 
+                    host: 'Ryan Scrolls', 
+                    time: '8:00am', 
+                    date: 'Nov 14', 
+                    place: 'Harbor Walk', 
+                    guests: '8',
+                    description: 'none',
+                    avatar: 'img/avatars/ryry.jpg'
+                }
+            ]
+        }
+    ];
 
-  return {
-    all: function() {
-      return events;
-    },
-    get: function(eventId) {
-      // Simple index lookup
-      return events[eventId];
+    return {
+        all: function() {
+            return events;
+        },
+        get: function(eventId) {
+            // Empty evt object that will be returned
+            var evt = {};
+            
+            eventId = parseInt(eventId);
+
+            if (eventId === NaN) {
+                return evt;
+            }
+
+            angular.forEach(events, function(value, key) {
+
+                angular.forEach(value.events, function(evt2, ekey) {
+                    if (evt2.id === eventId) {
+                        evt = evt2;
+                    }
+                });
+            });
+
+            return evt;
+        }
     }
-  }
 });
